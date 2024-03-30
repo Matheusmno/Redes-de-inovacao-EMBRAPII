7bde6f1bf9364c9995a53d3cd52760e67660ed7b
Matheusmno <mnomatheus@gmail.com>
2024-03-30 15:23:11 -0300

    maintenance for new excel table

--- dashboard.py
+++ dashboard.py
@@ -7,32 +7,32 @@ st.markdown("""
     <h1 style='text-align: center;'>Redes de inovação EMBRAPII</h1>
 """, unsafe_allow_html=True)
 # File upload in the main window
-uploaded_file = st.file_uploader("Escolha o arquivo Excel 'Papeis da rede':", type=["xlsx", "xlsm"])
+uploaded_file = st.file_uploader("Escolha o arquivo Excel 'Tabela Periódica - Excelência Operacional':", type=["xlsx", "xlsm"])
 
 # Check if a file is uploaded
 if uploaded_file is not None:
     # Pre processing
     data = pd.read_excel(uploaded_file, sheet_name='Dados', header=1)
-    data['Soma das métricas'] = data['Prospecção e Negociação'] + data['Gestão de Projetos/PI'] + data['Relacional/\nComunicação'] + data['Padronização da Gestão']
-    data.rename(columns={'Classificação': 'Papel de atuação em rede'}, inplace=True)
+    data['Soma das métricas'] = data['Impacto Potencial dos Resultados'] + data['Projetos de PD&I'] + data['Relações com empresas'] + data['QIM ver']
+    data.rename(columns={'Classificação': 'Classificação'}, inplace=True)
 
     
     # Sidebar with filters
     st.sidebar.title('Filtros')
-    selected_category = st.sidebar.selectbox('Papel da Rede', ["Diamante", "Ouro", "Prata"], index=0)
+    selected_category = st.sidebar.selectbox('Classificação', ["Diamante", "Ouro", "Prata"], index=0)
     sorted_unity = np.sort(data['Unidade EMBRAPII'].dropna().unique())
     selected_unity = st.sidebar.selectbox('Unidade', np.insert(sorted_unity, 0, "Todas", axis=0))
-    selected_dimension = st.sidebar.selectbox('Dimensão', ['Prospecção e Negociação', 'Gestão de Projetos/PI', 'Relacional/\nComunicação', 'Padronização da Gestão'])
+    selected_dimension = st.sidebar.selectbox('Dimensão', ['Impacto Potencial dos Resultados', 'Projetos de PD&I', 'Relações com empresas', 'QIM ver'])
     selected_tematicas = st.sidebar.multiselect('Temática', data['CLASSIFICAÇÃO TEMÁTICA EMBRAPII'].dropna().unique())
-    selected_tipoue = st.sidebar.multiselect('Tipo da UE', data['TIPO DE INSTITUIÇÃO'].dropna().unique())
+    #selected_tipoue = st.sidebar.multiselect('Tipo da UE', data['TIPO DE INSTITUIÇÃO'].dropna().unique())
 
     # Apply filters
     if 'Diamante' in selected_category:
-        filtered_data = data[data['Papel de atuação em rede'].isin(['DIAMANTE', 'OURO', 'PRATA', 'BRONZE'])]
+        filtered_data = data[data['Classificação'].isin(['DIAMANTE', 'OURO', 'PRATA', 'BRONZE'])]
     elif 'Ouro' in selected_category:
-        filtered_data = data[data['Papel de atuação em rede'].isin(['OURO', 'PRATA', 'BRONZE'])]
+        filtered_data = data[data['Classificação'].isin(['OURO', 'PRATA', 'BRONZE'])]
     else:
-        filtered_data = data[data['Papel de atuação em rede'].isin(['PRATA', 'BRONZE'])]
+        filtered_data = data[data['Classificação'].isin(['PRATA', 'BRONZE'])]
 
     if 'Todas' not in selected_unity:
         dimension_value = data.loc[data['Unidade EMBRAPII'] == selected_unity, selected_dimension].values[0]
@@ -40,8 +40,8 @@ if uploaded_file is not None:
 
     if selected_tematicas:
         filtered_data = filtered_data[filtered_data['CLASSIFICAÇÃO TEMÁTICA EMBRAPII'].isin(selected_tematicas)]
-    if selected_tipoue:
-        filtered_data = filtered_data[filtered_data['TIPO DE INSTITUIÇÃO'].isin(selected_tipoue)]
+    #if selected_tipoue:
+    #    filtered_data = filtered_data[filtered_data['TIPO DE INSTITUIÇÃO'].isin(selected_tipoue)]
 
     num_unidades = filtered_data['Unidade EMBRAPII'].count()
     
@@ -51,8 +51,8 @@ if uploaded_file is not None:
         
         # Scatter Plot
         color_discrete_map = {'DIAMANTE': 'rgb(185,242,255)', 'OURO': 'rgb(255,215,0)', 'PRATA': 'rgb(192,192,192)', 'BRONZE': 'rgb(184,115,51)'}
-        fig_scatter = px.scatter_3d(filtered_data, x='Prospecção e Negociação', y='Gestão de Projetos/PI', z='Relacional/\nComunicação',
-              color='Papel de atuação em rede', size='Soma das métricas',
+        fig_scatter = px.scatter_3d(filtered_data, x='Impacto Potencial dos Resultados', y='Projetos de PD&I', z='Relações com empresas',
+              color='Classificação', size='Soma das métricas',
               color_discrete_map=color_discrete_map,
               hover_name="Unidade EMBRAPII",
               custom_data=['Unidade EMBRAPII'])
@@ -62,12 +62,14 @@ if uploaded_file is not None:
         st.plotly_chart(fig_scatter, use_container_width=True)
         
         # Treemap
-        treemap_data = filtered_data.groupby(['Papel de atuação em rede']).size().reset_index(name='Count')
-        fig_treemap = px.treemap(treemap_data, path=['Papel de atuação em rede'], values='Count', color='Papel de atuação em rede', color_discrete_map=color_discrete_map)
+        treemap_data = filtered_data.groupby(['Classificação']).size().reset_index(name='Count')
+        fig_treemap = px.treemap(treemap_data, path=['Classificação'], values='Count', color='Classificação', color_discrete_map=color_discrete_map)
         st.plotly_chart(fig_treemap, use_container_width=True)
 
         # Display table with selected columns
-        selected_columns_data = filtered_data[['Unidade EMBRAPII', 'CIDADE', 'TIPO DE INSTITUIÇÃO', 'CLASSIFICAÇÃO TEMÁTICA EMBRAPII', 'Papel de atuação em rede']]
+        selected_columns_data = filtered_data[['Unidade EMBRAPII', 'CIDADE', 
+                                               #'TIPO DE INSTITUIÇÃO', 
+                                               'CLASSIFICAÇÃO TEMÁTICA EMBRAPII', 'Classificação']]
         st.markdown(selected_columns_data.to_html(index=False, escape=False), unsafe_allow_html=True)
     else:
         st.warning("Não há registros para os filtros selecionados.")
@@ -81,4 +83,4 @@ if uploaded_file is not None:
     """.format(num_unidades), unsafe_allow_html=True)
 
 else:
-    st.warning("Por favor, faça o upload da última versão do arquivo Excel 'Papeis da rede'.")
+    st.warning("Por favor, faça o upload da última versão do arquivo Excel 'Tabela Periódica - Excelência Operacional'.")
